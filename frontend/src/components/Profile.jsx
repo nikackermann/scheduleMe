6c29a65889044e60cc95f63931db29de549ff1a1
Nik Ackermann <nik.ackermann@gmail.com>
2018-04-23 22:59:14 -0400

    updated profile more and replaced 'class' with 'className'

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,85 +1,46 @@
 import React, { Component } from 'react';
 
 const Profile = () => (
-  <div class="container">
-    <h1 class="title has-text-weight-light">
+  <div className="container">
+    <h1 className="title has-text-weight-light">
       Hello, <span className="">'Name here!'</span>
     </h1>
     {/*Profile Tab*/}
-    <div class="tile is-ancestor">
-      <div class="tile is-4 is-vertical is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Major</p>
-          <span>Computer Science (CS-General)</span>C
+    <div className="tile is-ancestor">
+      <div className="tile is-4 is-vertical is-parent">
+        <div className="tile is-child box">
+          <p className="title is-4">Major</p>
+          <span>Computer Science (CS-General)</span>
         </div>
-        <div class="tile is-child box">
-          <p class="title is-4">Progress</p>
+        <div className="tile is-child box">
+          <p className="title is-4">Progress</p>
           <p>
             <p>Estimated Progress</p>
-            <progress class="progress is-primary" value="15" max="100">
+            <progress className="progress is-primary" value="15" max="100">
               30%
             </progress>
           </p>
         </div>
       </div>
-      <div class="tile is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Completed Courses</p>
-          <p>Courses you've taken will appear here.</p>
-        </div>
-      </div>
-    </div>
-
-    {/*Preferences Tab*/}
-    <div class="tile is-ancestor">
-      <div class="tile is-4 is-vertical is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Major</p>
-          <p>Computer Science (CS-General)</p>
-        </div>
-        <div class="tile is-child box">
-          <p class="title is-4">Progress</p>
-          <p>
-            <p>Estimated Progress</p>
-            <progress class="progress is-primary" value="15" max="100">
-              30%
-            </progress>
-          </p>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Let's talk about you</p>
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <p className="title is-4">Let's talk about you</p>
           <p>Here are your preferred times and teachers</p>
         </div>
       </div>
     </div>
 
-    {/*Edit Profile Tab*/}
-    <div class="tile is-ancestor">
-      <div class="tile is-4 is-vertical is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Major</p>
-          <p>Computer Science (CS-General)</p>
-        </div>
-        <div class="tile is-child box">
-          <p class="title is-4">Progress</p>
-          <p>
-            <p>Estimated Progress</p>
-            <progress class="progress is-primary" value="15" max="100">
-              30%
-            </progress>
-          </p>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <div class="tile is-child box">
-          <p class="title is-4">Edit Completed Courses</p>
+    {/*Preferences Tab*/}
+    <div className="tile is-ancestor">
+      <div className="tile is-4 is-vertical is-parent" />
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <p className="title is-4">Edit Completed Courses</p>
           <p>
             <h2>What classes have you taken already?</h2>
-            <div class="field">
-              <div class="control is-expanded">
-                <div class="select is-fullwidth">
+            <div className="field">
+              <div className="control is-expanded">
+                <div className="select is-fullwidth">
                   <select name="Course">
                     <option value="CSC">CSC</option>
                     <option value="MAT">MAT</option>
@@ -91,10 +52,21 @@ const Profile = () => (
               </div>
             </div>
 
-            <div class="control">
-              <button class="button is-primary">Submit</button>
+            <div className="control">
+              <button className="button is-primary">Submit</button>
             </div>
           </p>
+        </div>
+      </div>
+    </div>
+
+    {/*Edit Profile Tab*/}
+    <div className="tile is-ancestor">
+      <div className="tile is-4 is-vertical is-parent" />
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <p className="title is-4">Completed Courses</p>
+          <p>Courses you've taken will appear here.</p>
         </div>
       </div>
     </div>
